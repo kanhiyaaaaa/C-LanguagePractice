07b4b8f88eba1a86a4c328fd1a07b8d4cb5fa993
kanhiyaaaaa <dhingrakanhaiya4@gmail.com>
2024-09-16 18:39:46 +0530

    Add files via upload

---
+++ Code/test.cpp
@@ -0,0 +1,21 @@
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return slow;
+    }
+};
