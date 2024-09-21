69a9bdd75cb18f49a299ff19f318763f28ae44fb
kanhiyaaaaa <dhingrakanhaiya4@gmail.com>
2024-09-21 13:20:39 +0530

    Add files via upload

---
+++ reversestrusestack.cpp
@@ -0,0 +1,21 @@
+//reverse a string using stack
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    string s="love";
+    stack<char>st;
+    for(int i=0;i<s.length();i++){
+        char ch =s[i];
+        st.push(ch);
+    }
+    string ans="";
+    while(!st.empty()){
+        char ch =st.top();
+        ans.push_back(ch);
+        st.pop();
+    }
+    cout<<"answer is:"<<ans<<endl;
+    return 0;
+}
